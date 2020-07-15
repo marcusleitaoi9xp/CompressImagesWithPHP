70bffe519c8ec4b49cec2c5d88d952c8bf87be4e
Marcus Leitão <marcusvinicius.leitao@i9xp.com.br>
2020-07-15 18:14:10 -0300

    adição de exemplo de uso

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     {
         /**
         * @param @files: array(array("name" => "nome-do-arquivo.jpg", "url" => "http://www.url.com.br/do/arquivo.jpg"))
-        * @param @zipName: string com o nome do arquivo zip (sem a extenso ".zip") que o cliente far download
+        * @param @zipName: string com o nome do arquivo zip (sem a extenso ".zip") que o usuário fará o download
         */
         public function compressZip ($files = array(), $zipName = 'file')
         {
@@ -37,5 +37,24 @@
                     unlink($fullPath);
                 }
             }
+        }
+
+        public function exemploUso() {
+            $files = array(
+                array(
+                    "name" => "praia",
+                    "url" => "https://viajantecomum.com/wp-content/uploads/2019/04/garca-torta-953x715.jpg"
+                ),
+                array(
+                    "name" => "morangos",
+                    "url" => "https://conteudo.imguol.com.br/c/entretenimento/78/2018/02/28/morango-1519823853148_v2_1920x1280.jpg"
+                ),
+                array(
+                    "name" => "caviar",
+                    "url" => "https://s2.glbimg.com/MVaVZyFOdnGDBgVzYDkszBAI7yA=/512x320/smart/e.glbimg.com/og/ed/f/original/2019/05/01/gq095-gostocaviar-01.jpg"
+                )
+            );
+
+            $this->compressZip($files, "arquivo.zip");
         }
     }
